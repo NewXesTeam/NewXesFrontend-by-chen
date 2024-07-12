611f6ec2c870542ee1e3518061a3676bb29ea0eb
lrs2187 <liurongshuo2022@outlook.com>
2024-07-12 16:10:26 +0800

    create discover page

--- src/static/js/project.js
+++ src/static/js/project.js
@@ -1,9 +1,10 @@
-const project_follows = () => {
+const project_follows = (box_id) => {
     fetch("/api/index/works/follows")
     .then(e => e.json())
     .then(data => {
         data = data.data;
-        let box = document.getElementById("project-follows")
+        let box = document.getElementById(box_id)
+        let full_content = ""
         data.forEach(project => {
             let link = `https://code.xueersi.com/home/project/detail?lang=code&pid=${project.id}&version=${project.version}&langType=${project.lang}`;
             let title = project.name
@@ -12,8 +13,31 @@ const project_follows = () => {
             let cover = project.thumbnail;
             let infos = `👀${project.views} 👍${project.likes}`;
             let created_at = project.created_at;
-            let html = `<div class="col"><div class="card m-3"><a href="${link}" class="text-decoration-none"><img src="${cover}" class="card-img-top project-card-img" alt="${title}"><div class="card-body"><h5 class="card-title">${title}</h5><p class="card-text" style="transform: rotate(0);"><a href="${author_url}">${author}</a> ${infos}</p><p class="card-text"><small class="text-body-secondary">${created_at}</small></p></div></a></div></div>`;
-            box.innerHTML = box.innerHTML + html;
+            let content = `<div class="col"><div class="card m-3"><a href="${link}" class="text-decoration-none"><img src="${cover}" class="card-img-top project-card-img" alt="${title}"><div class="card-body"><h5 class="card-title">${title}</h5><p class="card-text" style="transform: rotate(0);"><a href="${author_url}">${author}</a> ${infos}</p><p class="card-text"><small class="text-body-secondary">${created_at}</small></p></div></a></div></div>`;
+            full_content = full_content + content;
         });
+        box.innerHTML = full_content;
+    })
+}
+
+const project_discover_latest = (box_id, page, per_page) => {
+    fetch(`/api/works/latest?page=${page}&per_page=${per_page}`)
+    .then(e => e.json())
+    .then(data => {
+        data = data.data;
+        let box = document.getElementById(box_id)
+        let full_content = ""
+        data.forEach(project => {
+            let link = `https://code.xueersi.com/home/project/detail?lang=code&pid=${project.id}&version=${project.version}&langType=${project.lang}`;
+            let title = project.name
+            let author = project.username;
+            let author_url = `https://code.xueersi.com/space/${project.user_id}`;
+            let cover = project.thumbnail;
+            let infos = `👀${project.views} 👍${project.likes}`;
+            let created_at = project.created_at;
+            let content = `<div class="col"><div class="card m-3"><a href="${link}" class="text-decoration-none"><img src="${cover}" class="card-img-top project-card-img" alt="${title}"><div class="card-body"><h5 class="card-title">${title}</h5><p class="card-text" style="transform: rotate(0);"><a href="${author_url}">${author}</a> ${infos}</p><p class="card-text"><small class="text-body-secondary">${created_at}</small></p></div></a></div></div>`;
+            full_content = full_content + content;
+        });
+        box.innerHTML = full_content;
     })
 }
